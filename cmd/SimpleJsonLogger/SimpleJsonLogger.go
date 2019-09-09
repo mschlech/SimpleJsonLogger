8f46958806030c266b1040df6042e2bf1442df44
marcusschlechter <marcus.schlechter@yahoo.de>
2019-09-09 15:00:35 +0200

    working simple make

---
+++ cmd/SimpleJsonLogger/SimpleJsonLogger.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/mschlech/SimpleJsonLogger/private/app/SimpleJsonLogger"
+)
+
+func main() {
+
+	app.StartJsonLogger()
+}
---
+++ private/app/SimpleJsonLogger/SimpleJsonLoggerHttpLooper_test.go
@@ -0,0 +1,18 @@
+package app
+
+import (
+       "log"
+       "testing"
+)
+
+func TestStartJsonLogger(t *testing.T) {
+	log.Print("Test passed")
+}
+
+func encodePostRequestFromSourceTest() {
+
+}
+
+func healtCheckTest() {
+
+}
